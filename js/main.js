c6a987875ba8efe6a99b34d91d5e6d2150230ab0
kendallLH <kendallhaberer@comcast.net>
2016-11-12 20:16:59 -0600

    little touchups

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-/*
-$('html').on('load', function(){
+
+$(window).ready(function(){
 	var quote = $.ajax({
 		url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
 		type: 'POST',
 		data: {cat:'famous'},
 		dataType: 'json',
 		success: function(data) {
-			document.getElementById("quote_here").innerHTML = data.quote;
-			document.getElementById("author").innerHTML = data.author;
+			document.getElementById("quote_here").innerHTML = "\"" + data.quote + "\"";
+			document.getElementById("author").innerHTML = "-" + data.author;
 		},
 		beforeSend: function(xhr) {
     		xhr.setRequestHeader("X-Mashape-Authorization", "fDL01xsfZsmsh1h3EtKTxL794D7Gp1t994cjsnUcS6mNnm4jnB"); // Enter here your Mashape key
 		}
 	});
 });
-*/
+
 
 function generate_quote() {
 	var quote = $.ajax({
@@ -24,7 +24,7 @@ function generate_quote() {
 		dataType: 'json',
 		success: function(data) {
 			document.getElementById("quote_here").innerHTML = "\"" + data.quote + "\"";
-			document.getElementById("author").innerHTML = data.author;
+			document.getElementById("author").innerHTML = "-" + data.author;
 		},
 		beforeSend: function(xhr) {
     		xhr.setRequestHeader("X-Mashape-Authorization", "fDL01xsfZsmsh1h3EtKTxL794D7Gp1t994cjsnUcS6mNnm4jnB"); // Enter here your Mashape key
