f306a7ae28469b6f8d5d899f9a06f8e48b61790b
kendallLH <kendallhaberer@comcast.net>
2016-11-18 16:36:54 -0600

    made some changes

--- js/main_cats.js
+++ js/main_cats.js
@@ -4,18 +4,21 @@ $(window).ready(function(){
 });
 
 function generate_cats() {
-	var num = Math.floor(Math.random()*100) ;
 	$.ajax({
 		url: 'https://montanaflynn-cat-overflow.p.mashape.com/',
 		type: 'GET',
-		data: {limit:'1', offset: num},
+		data: {limit:'100', offset: 0},
 		dataType: 'text',
 		success: function(data) {
-			document.getElementById("cat_pic").src = data ;
-			//alert(data) ;
+			//var url = 'https://catoverflow.com/cats/' + 
+
+			var cats = data.split("\n") ;
+			var pickCat = cats[Math.floor(Math.random()*100)] ;
+			document.getElementById("cat_pic").src = pickCat ;
 		},
 		beforeSend: function(xhr) {
     		xhr.setRequestHeader("X-Mashape-Authorization", "fDL01xsfZsmsh1h3EtKTxL794D7Gp1t994cjsnUcS6mNnm4jnB"); // Enter here your Mashape key
 		}
 	});
+	return ;
 }
