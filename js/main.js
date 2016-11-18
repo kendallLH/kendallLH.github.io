2092aaedd561dfa7cb0c2c1b1fd5a3ebbb466ccb
kendallLH <kendallhaberer@comcast.net>
2016-11-18 13:44:00 -0600

    cat facts added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+//Famous quotes (original)
 $(window).ready(function(){
 	var quote = $.ajax({
 		url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
@@ -33,16 +33,23 @@ function generate_quote() {
 }
 
 
+//IDEA FOR CAT PAGE: FUNNY PICTURES W/ CAT FACTS UNDER THEM
+
  
-/*
+/* //they said so freemium
 $(window).ready(function(){
 	var quote = $.ajax({
-		url: 'https://www.notableandquotable.com/RandomQuote.asmx?',
+		url: 'https://theysaidso.p.mashape.com/quote',
 		type: 'POST',
-		//data: {cat:'Inspirational Quote'},
-		dataType: 'xml',
+		data: {category:'inspirational'},
+		dataType: 'json',
 		success: function(data) {
 			//$(xml).find()
+			document.getElementById("quote_here").innerHTML = "\"" + data.quote + "\"";
+			document.getElementById("author").innerHTML = "-" + data.author;
+		},
+		beforeSend: function(xhr) {
+    		xhr.setRequestHeader("X-Mashape-Authorization", "fDL01xsfZsmsh1h3EtKTxL794D7Gp1t994cjsnUcS6mNnm4jnB");
 		}
 	});
 });
@@ -50,10 +57,49 @@ $(window).ready(function(){
 
 function generate_quote() {
 	var quote = $.ajax({
-		url: 'https://www.notableandquotable.com/RandomQuote.asmx?',
+		url: 'https://theysaidso.p.mashape.com/quote',
 		type: 'POST',
-		//data: {cat:'Inspirational Quote'},
-		dataType: 'xml',
+		data: {category:'inspiration'},
+		dataType: 'json',
+		success: function(data) {
+			document.getElementById("quote_here").innerHTML = "\"" + data.quote + "\"";
+			document.getElementById("author").innerHTML = "-" + data.author;
+		},
+		beforeSend: function(xhr) {
+    		xhr.setRequestHeader("X-Mashape-Authorization", "fDL01xsfZsmsh1h3EtKTxL794D7Gp1t994cjsnUcS6mNnm4jnB");
+		}
+	});
+}
+*/
+
+/*
+$(window).ready(function(){
+	
+	var quote = $.ajax({
+		url: 'http://api.forismatic.com/api/1.0/?method=getQuote&key=&format=jsonp&language=en&jsonp=callbackfunc()',
+		type: 'POST',
+		//data: {method:'getQuote', key: '', format: 'jsonp', language: 'en', jsonp: 'callbackfunc()'},
+		dataType: 'text',
+		success: function(data) {
+			//$(xml).find()
+			//document.getElementById("quote_here").innerHTML = "\"" + data.quote + "\"";
+			//document.getElementById("author").innerHTML = "-" + data.author;
+		}
+	});
+	
+});
+
+function callbackfunc(data) {
+	alert(data) ;
+}
+
+
+function generate_quote() {
+	var quote = $.ajax({
+		url: 'http://api.forismatic.com/api/1.0/',
+		type: 'POST',
+		data: {method:'getQuote', key: '', format: 'jsonp', language: 'en'},
+		dataType: 'text',
 		success: function(data) {
 			//document.getElementById("quote_here").innerHTML = "\"" + data.quote + "\"";
 			//document.getElementById("author").innerHTML = "-" + data.author;
